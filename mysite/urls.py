b078b446b123cae12454a869f0cfc55b79d4955c
Agathonejriii <agathonek@gmail.com>
2025-11-01 16:52:06 +0300

    Fix: Convert PosixPath to string in static file serving

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,6 @@ mysite URL Configuration - Serving Vite React Frontend
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -18,12 +15,8 @@ urlpatterns = [
     path('api/students/', include('students.urls')),
 ]
 
-# Serve React assets (Vite uses /assets/ path)
-urlpatterns += [
-    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT + '/assets'}),
-]
-
 # Serve React frontend - Catch-all for all other routes
+# WhiteNoise will automatically handle static files including assets/
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
